86ce0da8461376056135a59de5cbf62e4a2de180
AJ <necessarylion@gmail.com>
2020-07-21 10:15:38 +0700

    Create Fpdf class

---
+++ src/font/Kinnari.php
@@ -0,0 +1,26 @@
+<?php
+$type = 'TrueType';
+$name = 'Kinnari';
+$desc = array('Ascent'=>800,'Descent'=>-200,'CapHeight'=>800,'Flags'=>32,'FontBBox'=>'[-708 -493 1514 1260]','ItalicAngle'=>0,'StemV'=>70,'MissingWidth'=>364);
+$up = -82;
+$ut = 25;
+$cw = array(
+	chr(0)=>364,chr(1)=>364,chr(2)=>364,chr(3)=>364,chr(4)=>364,chr(5)=>364,chr(6)=>364,chr(7)=>364,chr(8)=>364,chr(9)=>364,chr(10)=>364,chr(11)=>364,chr(12)=>364,chr(13)=>364,chr(14)=>364,chr(15)=>364,chr(16)=>364,chr(17)=>364,chr(18)=>364,chr(19)=>364,chr(20)=>364,chr(21)=>364,
+	chr(22)=>364,chr(23)=>364,chr(24)=>364,chr(25)=>364,chr(26)=>364,chr(27)=>364,chr(28)=>364,chr(29)=>364,chr(30)=>364,chr(31)=>364,' '=>384,'!'=>333,'"'=>407,'#'=>499,'$'=>499,'%'=>832,'&'=>777,'\''=>179,'('=>333,')'=>333,'*'=>499,'+'=>563,
+	','=>250,'-'=>333,'.'=>250,'/'=>278,'0'=>537,'1'=>537,'2'=>537,'3'=>537,'4'=>537,'5'=>537,'6'=>537,'7'=>537,'8'=>537,'9'=>537,':'=>278,';'=>278,'<'=>563,'='=>563,'>'=>563,'?'=>443,'@'=>920,'A'=>721,
+	'B'=>666,'C'=>666,'D'=>721,'E'=>609,'F'=>556,'G'=>721,'H'=>721,'I'=>333,'J'=>388,'K'=>721,'L'=>609,'M'=>887,'N'=>721,'O'=>721,'P'=>556,'Q'=>721,'R'=>666,'S'=>556,'T'=>610,'U'=>721,'V'=>721,'W'=>943,
+	'X'=>721,'Y'=>721,'Z'=>609,'['=>333,'\\'=>278,']'=>333,'^'=>468,'_'=>499,'`'=>333,'a'=>443,'b'=>499,'c'=>443,'d'=>499,'e'=>443,'f'=>332,'g'=>499,'h'=>499,'i'=>278,'j'=>278,'k'=>499,'l'=>278,'m'=>777,
+	'n'=>499,'o'=>499,'p'=>499,'q'=>499,'r'=>333,'s'=>388,'t'=>278,'u'=>499,'v'=>499,'w'=>721,'x'=>499,'y'=>499,'z'=>443,'{'=>479,'|'=>199,'}'=>479,'~'=>540,chr(127)=>364,chr(128)=>364,chr(129)=>364,chr(130)=>364,chr(131)=>364,
+	chr(132)=>364,chr(133)=>364,chr(134)=>364,chr(135)=>364,chr(136)=>364,chr(137)=>364,chr(138)=>364,chr(139)=>364,chr(140)=>364,chr(141)=>364,chr(142)=>364,chr(143)=>364,chr(144)=>364,chr(145)=>364,chr(146)=>364,chr(147)=>364,chr(148)=>364,chr(149)=>364,chr(150)=>364,chr(151)=>364,chr(152)=>364,chr(153)=>364,
+	chr(154)=>364,chr(155)=>364,chr(156)=>364,chr(157)=>364,chr(158)=>364,chr(159)=>364,chr(160)=>384,chr(161)=>602,chr(162)=>585,chr(163)=>634,chr(164)=>589,chr(165)=>588,chr(166)=>757,chr(167)=>479,chr(168)=>565,chr(169)=>615,chr(170)=>583,chr(171)=>649,chr(172)=>815,chr(173)=>800,chr(174)=>612,chr(175)=>612,
+	chr(176)=>508,chr(177)=>780,chr(178)=>801,chr(179)=>800,chr(180)=>609,chr(181)=>609,chr(182)=>602,chr(183)=>655,chr(184)=>508,chr(185)=>663,chr(186)=>649,chr(187)=>665,chr(188)=>649,chr(189)=>665,chr(190)=>723,chr(191)=>723,chr(192)=>619,chr(193)=>646,chr(194)=>580,chr(195)=>462,chr(196)=>597,chr(197)=>579,
+	chr(198)=>611,chr(199)=>505,chr(200)=>589,chr(201)=>665,chr(202)=>579,chr(203)=>654,chr(204)=>715,chr(205)=>580,chr(206)=>580,chr(207)=>539,chr(208)=>460,chr(209)=>0,chr(210)=>427,chr(211)=>427,chr(212)=>0,chr(213)=>0,chr(214)=>0,chr(215)=>0,chr(216)=>0,chr(217)=>0,chr(218)=>0,chr(219)=>364,
+	chr(220)=>364,chr(221)=>364,chr(222)=>364,chr(223)=>752,chr(224)=>337,chr(225)=>609,chr(226)=>331,chr(227)=>362,chr(228)=>403,chr(229)=>304,chr(230)=>703,chr(231)=>0,chr(232)=>0,chr(233)=>0,chr(234)=>0,chr(235)=>0,chr(236)=>0,chr(237)=>0,chr(238)=>0,chr(239)=>737,chr(240)=>645,chr(241)=>645,
+	chr(242)=>645,chr(243)=>645,chr(244)=>645,chr(245)=>645,chr(246)=>645,chr(247)=>645,chr(248)=>645,chr(249)=>645,chr(250)=>731,chr(251)=>1474,chr(252)=>364,chr(253)=>364,chr(254)=>364,chr(255)=>364);
+$enc = 'ISO-8859-11';
+$diff = '128 /.notdef 130 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 142 /.notdef 145 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 158 /.notdef /.notdef 161 /kokaithai /khokhaithai /khokhuatthai /khokhwaithai /khokhonthai /khorakhangthai /ngonguthai /chochanthai /chochingthai /chochangthai /sosothai /chochoethai /yoyingthai /dochadathai /topatakthai /thothanthai /thonangmonthothai /thophuthaothai /nonenthai /dodekthai /totaothai /thothungthai /thothahanthai /thothongthai /nonuthai /bobaimaithai /poplathai /phophungthai /fofathai /phophanthai /fofanthai /phosamphaothai /momathai /yoyakthai /roruathai /ruthai /lolingthai /luthai /wowaenthai /sosalathai /sorusithai /sosuathai /hohipthai /lochulathai /oangthai /honokhukthai /paiyannoithai /saraathai /maihanakatthai /saraaathai /saraamthai /saraithai /saraiithai /sarauethai /saraueethai /sarauthai /sarauuthai /phinthuthai /.notdef /.notdef /.notdef /.notdef /bahtthai /saraethai /saraaethai /saraothai /saraaimaimuanthai /saraaimaimalaithai /lakkhangyaothai /maiyamokthai /maitaikhuthai /maiekthai /maithothai /maitrithai /maichattawathai /thanthakhatthai /nikhahitthai /yamakkanthai /fongmanthai /zerothai /onethai /twothai /threethai /fourthai /fivethai /sixthai /seventhai /eightthai /ninethai /angkhankhuthai /khomutthai /.notdef /.notdef /.notdef /.notdef';
+$uv = array(0=>array(0,161),161=>array(3585,58),223=>array(3647,29));
+$file = 'Kinnari.z';
+$originalsize = 58716;
+$subsetted = true;
+?>
